b296bb01ce256d81719149feff254d7e9ce3ae60
QW3RTZUI0P <martinjurk102@gmail.com>
2021-02-06 14:39:11 +0000

    0.0.0
    - added basic functionality (activation noise works, if the .wav file has been placed in the proper place (node-sonos-http-api/static/clips); tts also works)
    - added basic settings
    - still dependant on the Node Sonos HTTP API Server (https://github.com/jishi/node-sonos-http-api)

--- __init__.py
+++ __init__.py
@@ -1,13 +1,43 @@
 from mycroft import MycroftSkill, intent_file_handler
+# used to communicate with the Sonos Node JS Server
+import requests
 
 
 class SpeechOverSonos(MycroftSkill):
+
+    # important values for the skill to function
+    # can be edited in the normal Mycroft Skill settings on home.mycroft.ai
+    sonos_server_ip = ""
+    room = ""
+    url = ""
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('sonos.over.speech.intent')
-    def handle_sonos_over_speech(self, message):
-        self.speak_dialog('sonos.over.speech')
+    def initialize(self):
+        SpeechOverSonos.sonos_server_ip = self.settings.get("sonos_server_ip")
+        SpeechOverSonos.room = self.settings.get("room")
+        # putting the values in str() is necessary
+        SpeechOverSonos.url = "http://" + str(SpeechOverSonos.sonos_server_ip) + ":5005/" + str(SpeechOverSonos.room) + "/"
+
+        # connects with the mycroft-playback-control messagebus
+        self.add_event("recognizer_loop:wakeword", self.activation_confirmation_noise_on_sonos)
+        self.add_event("speak", self.output_speech_on_sonos)
+
+
+    # general function to call the sonos api
+    def sonos_api(action = ""):
+        requests.get(SpeechOverSonos.url + str(action))
+
+    # function to make the activation noise on the Sonos speaker
+    # requires the file start_listening.wav in the folder node-sonos-http-api/static/clips on the machine where the node js server runs
+    # the start_listening.wav file can be found in mycroft-core/mycroft/res/snd/start_listening.wav
+    def activation_confirmation_noise_on_sonos(self, message):
+        SpeechOverSonos.sonos_api(action = "clip/start_listening.wav/45")
+
+    # function to output speech over the Sonos speaker using the TTS feature of the node js sonos server
+    def output_speech_on_sonos(self, message):
+        SonosMusicController.sonos_api(action = "say/" + str(message.data.get("utterance")) + "/de-de")
 
 
 def create_skill():
